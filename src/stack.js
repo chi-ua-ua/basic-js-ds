eb640d98299f67e014929be0aa96932119b9292e
chi-ua-ua <chi-ua-ua_74@mail.ru>
2023-04-17 19:37:20 +0500

    stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  arr = [];
+
+  push(element) {
+    this.arr.push(element);
+    return this;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let lastIndex = this.arr.length;
+   return this.arr[lastIndex - 1];
   }
 }
 
